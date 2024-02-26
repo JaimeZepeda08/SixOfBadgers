fc137b141e6c678c040f57c879e8dbb41a4c6f5c
JAIME ZEPEDA PINA <zepedapina@wisc.edu>
2024-02-25 18:42:46 -0600

    Added Card and Hand UI components

--- euchreGame/frontend/src/App.js
+++ euchreGame/frontend/src/App.js
@@ -1,5 +1,20 @@
+import Hand from "./components/Hand";
+
 function App() {
-  return <h1>Frontend</h1>;
+  const cards = [
+    { suit: "hearts", value: "2" },
+    { suit: "diamonds", value: "K" },
+    { suit: "clubs", value: "J" },
+    { suit: "spades", value: "10" },
+    { suit: "hearts", value: "Q" },
+    { suit: "diamonds", value: "K" },
+  ];
+
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <Hand cards={cards} />
+    </div>
+  );
 }
 
 export default App;
