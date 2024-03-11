ff76bf898f43d089ce3bd8220c0cf33e13103a51
JAIME ZEPEDA PINA <zepedapina@wisc.edu>
2024-03-10 20:20:16 -0500

    Added option to show opponent's cards

--- euchreGame/frontend/src/app/page.tsx
+++ euchreGame/frontend/src/app/page.tsx
@@ -1,12 +1,10 @@
-import {getHello} from "@/lib/userService";
+import { getHello } from "@/lib/userService";
 
 export default function Home() {
-    const hello = getHello();
-    return (
-        <div className={'flex flex-col items-center h-full'}>
-            <div className={'flex justify-center w-full pt-4'}>
-                {hello}
-            </div>
-        </div>
-    );
+  const hello = getHello();
+  return (
+    <div className={"flex flex-col items-center h-full"}>
+      <div className={"flex justify-center w-full pt-4"}>{hello}</div>
+    </div>
+  );
 }
